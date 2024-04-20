b60b8984fbc035813fa9931bb6b52f2c7f8b01a1
TheFaheem <35933338+FaheemOnHub@users.noreply.github.com>
2024-04-20 23:34:50 +0530

    Update general.py

--- model/ultralytics-yolov5-5eb7f7d/utils/general.py
+++ model/ultralytics-yolov5-5eb7f7d/utils/general.py
@@ -35,7 +35,8 @@ import pkg_resources as pkg
 import torch
 import torchvision
 import yaml
-from ultralytics.yolo.utils.checks import check_requirements
+import ultralytics
+from ultralytics.utils.checks import check_requirements
 
 from utils import TryExcept, emojis
 from utils.downloads import curl_download, gsutil_getsize
